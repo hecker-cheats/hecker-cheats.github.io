5eae2478db2d99eff77ee1c88fc93c2e169634d4
hecker-cheats <88326347+hecker-cheats@users.noreply.github.com>
2021-08-19 18:44:12 +0200

    Update config.lua

--- heckers_hub/config.lua
+++ heckers_hub/config.lua
@@ -10,7 +10,7 @@ if game.PlaceId == 5535087806 then
   loadstring(game:HttpGet('https://hecker-cheats.github.io/heckers_hub/games/tapping_mania.lua', true))()
 else
   game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Hecker's Hub Loaded!";
-    Text = 'For Executor: '.._G.exploit;
+    Title = "Hecker's Hub No Loaded";
+    Text = "Game not supported.";
   })
 end
