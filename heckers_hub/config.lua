a05aa641aee94bfc837b4c323a0a9ec1ca981001
hecker-cheats <88326347+hecker-cheats@users.noreply.github.com>
2021-08-19 17:59:09 +0200

    Update config.lua

--- heckers_hub/config.lua
+++ heckers_hub/config.lua
@@ -1,4 +1,4 @@
 if game.PlaceId == 5535087806 then
   print('tapping mania detected.')
-  --scr
+  loadstring(game:HttpGet('https://hecker-cheats.github.io/heckers_hub/games/tapping_mania.lua', true))()
 end
