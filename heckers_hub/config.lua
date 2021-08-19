6438251ad8ba0f8fe45047dcacd61c18852a2707
hecker-cheats <88326347+hecker-cheats@users.noreply.github.com>
2021-08-19 18:41:11 +0200

    Update config.lua

--- heckers_hub/config.lua
+++ heckers_hub/config.lua
@@ -1,4 +1,11 @@
 if game.PlaceId == 5535087806 then
   print('tapping mania detected.')
+  if identifyexecutor then
+    _G.exploit = identifyexecutor();
+  end;
+  game:GetService("StarterGui"):SetCore("SendNotification", {
+	  Title = "Hecker's Hub Loaded!";
+	  Text = 'For Executor: '.._G.exploit;
+  })
   loadstring(game:HttpGet('https://hecker-cheats.github.io/heckers_hub/games/tapping_mania.lua', true))()
 end
