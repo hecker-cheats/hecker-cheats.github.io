0f74f2d8d65ed43dde7c3a6f4215e1d7b143f854
hecker-cheats <88326347+hecker-cheats@users.noreply.github.com>
2021-08-20 13:58:24 +0200

    Update config.lua

--- heckers_hub/config.lua
+++ heckers_hub/config.lua
@@ -8,6 +8,16 @@ if game.PlaceId == 5535087806 then
 	  Text = 'For Executor: '.._G.exploit;
   })
   loadstring(game:HttpGet('https://hecker-cheats.github.io/heckers_hub/games/tapping_mania.lua', true))()
+elseif game.PlaceId==155615604 then
+  print('prison life detected.')
+  if identifyexecutor then
+    _G.exploit = identifyexecutor();
+  end;
+  game:GetService("StarterGui"):SetCore("SendNotification", {
+	  Title = "Hecker's Hub Loaded!";
+	  Text = 'For Executor: '.._G.exploit;
+  })
+  loadstring(game:HttpGet('https://hecker-cheats.github.io/heckers_hub/games/prison_life.lua', true))()
 else
   game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Hecker's Hub No Loaded";
